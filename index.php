899f7a045d5804e7eeb7115fa1d609393e0a70c4
Birger Evansson <bibbevidio@gmail.com>
2019-09-04 17:44:53 +0200

    bank lol

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>MonetBank</title>
+		<link rel="stylesheet" type="text/css" href="style.css" />
+		<meta name="description" content="description">
+	</head>
+
+	<body>
+		<div id="container">
+			<div id="header">
+				<h1>MonetBank</h1>
+				<h5>monetize your monets!</h5>
+			</div>
+			
+			<div id="content">
+				<div id="nav">
+					<ul>
+						<li><a href="/login.php" class="link_button">Login</a></li>
+					</ul>
+				</div>
+
+				<div id="page">
+					<H2>The most stable currency in the world!</H2>
+					<p>Since no one uses this worthless currency,<br />
+					it's practicly useless.<br />
+					due to the low fluxuation rates the<br /> currency is very stable; stuck at 0%<br />
+					Invest in monets today, tiny price for you!</p>
+
+					<H2>Happy Customers! Happy Faces!</H2>
+				</div>
+			</div>
+			
+			<div id="footer">
+				<h5>Mord Och Utpressning AB&copy; 2019</h5>
+			</div>
+			
+		</div>
+	</body>
+
+</html>
